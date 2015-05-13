92ecf1e7e5ff900b869d893933ef7103dc5fe13f
Andrew Morgan <andrewmile@gmail.com>
2015-05-13 16:20:33 -0400

    namespacing

--- spec/andrewmile/FMHash/FMHashSpec.php
+++ spec/andrewmile/FMHash/FMHashSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace spec;
+namespace spec\andrewmile\FMHash;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -9,7 +9,7 @@ class FMHashSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('FMHash');
+        $this->shouldHaveType('andrewmile\FMHash\FMHash');
     }
 
     function it_creates_a_hash_from_a_key_value_pair()
--- src/andrewmile/FMHash/FMHash.php
+++ src/andrewmile/FMHash/FMHash.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace andrewmile\FMHash;
 
 class FMHash {
 	
